83fd8c839b1c1069a88f673edff73b784c721ec1
M Haikal Syah alam <mhaikalsyahalam40@gmail.com>
2026-09-17 11:53:28 +0700

    feat: add product data layer

---
+++ products.php
@@ -0,0 +1,28 @@
+<?php
+
+$products = [
+    [
+        "id" => 1,
+        "nama" => "Laptop",
+        "kategori" => "Elektronik",
+        "harga" => 7500000,
+        "stok" => 5,
+        "deskripsi" => "Laptop untuk kebutuhan kerja dan belajar."
+    ],
+    [
+        "id" => 2,
+        "nama" => "Mouse",
+        "kategori" => "Aksesoris",
+        "harga" => 150000,
+        "stok" => 2,
+        "deskripsi" => "Mouse wireless untuk komputer."
+    ],
+    [
+        "id" => 3,
+        "nama" => "Keyboard",
+        "kategori" => "Aksesoris",
+        "harga" => 300000,
+        "stok" => 8,
+        "deskripsi" => "Keyboard untuk komputer."
+    ]
+];
